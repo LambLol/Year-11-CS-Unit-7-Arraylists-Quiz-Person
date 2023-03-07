dbbaa31113575849723a4ff5f7f4c93fd41091eb
LambLol <ianl2025@student.cis.edu.hk>
2023-03-07 12:45:08 +0800

    .

--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,31 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        int size = allPersons.size();
+        //Sort by last name
+        for (int i = 0; i < allPersons.size(); i++){
+            if ((person.getLastName().compareTo(allPersons.get(i).getLastName()) < 0) && (allPersons.size() == size)){
+                allPersons.add(i, person);
+            }
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under = new ArrayList<>();
+        for(Person temp : allPersons){
+            if(temp.getAge() < 18){
+                under.add(temp);
+            }
+        }
+        return(under);
     }
 
     public boolean checkLastName(String lastName) {
-
+        for(Person temp : allPersons){
+            if(temp.getLastName().equals(lastName)){
+                return(true);
+            }
+        }
+        return(false);
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+
+public class Test {
+    public static void main(String[] args) {
+        String x = "Larson";
+        String y = "Jam";
+        System.out.println(x.compareTo(y));
+    }
+}
